c247e46832605380478072eb7af8fded5496fb50
Milca Alcantar <malcantar@matco.com.mx>
2022-04-21 17:37:14 +0000

    changes made from  theia lab environment

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value;
-    
-    result = p * ((r/100)*y)
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear() + parseInt(years);
+    amount = interest;
+
+    //Valida que se capture una cantidad
+    if(principal <= 0) {
+        alert("You must enter a positive amount");
+        var resultstr = "Not valid input";
+        document.getElementById("principal").focus();
+    } else {
+        var resultstr = "If you deposit \<span class=\"highlight-result\"\>" + principal + "\<\/span\>, \<br\>at an interest rate of \<span class=\"highlight-result\"\>" + rate + "%\<\/span\>\<br\>You will receive an amount of \<span class=\"highlight-result\"\>" + amount + ",\<\/span\>\<br\>in the year \<span class=\"highlight-result\"\>" + year +"\<\/span\>\<br\>";
+    }
+    //Despliega resultado
+    document.getElementById("result").innerHTML=resultstr;
 }
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
         
